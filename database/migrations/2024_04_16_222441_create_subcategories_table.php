41b0b9ca9ea5d5c6148c27b7322b75cb4f7f731d
saidur <saidur3419@gmail.com>
2024-06-17 02:27:18 +0100

    feat: added category

--- database/migrations/2024_04_16_222441_create_subcategories_table.php
+++ database/migrations/2024_04_16_222441_create_subcategories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id');
             $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->unsignedInteger('priority')->default(0);
             $table->timestamps();
 
