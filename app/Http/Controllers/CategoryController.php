d9e9f471500cac4ef333f3713264e09bb35e27b7
saidur <saidur3419@gmail.com>
2024-07-06 17:57:36 +0100

    fix: added mappedProduct function in product model to reuse

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
+use App\Services\HashIdService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -23,26 +24,13 @@ class CategoryController extends Controller
         }
         $products = $products->paginate(14);
 
-        $products->transform(function ($product) {
-            return (object) [
-                'id' => $product->id,
-                'name' => $product->name,
-                'image' => $product->images->first()->image_url ?? null,
-                'category' => $product->category->name,
-                'category_slug' => $product->category->slug,
-                'subcategory' => $product->subcategory->name ?? null,
-                'brand' => $product->brand->name ?? null,
-                'price' => $product->price,
-                'sale_price' => $product->sale_price,
-                'offer_percentage' => $product->offer_percentage,
-                'rating' => round($product->reviews->avg('rating') ?? 0),
-                'reviews' => $product->reviews->count() ?? 0,
-            ];
-        });
-
         if (! $products) {
             abort(404);
         }
+
+        $products->transform(function ($product) {
+            return app(Product::class)->mappedProduct($product);
+        });
 
         return Inertia::render('Home/Category/Show', compact('products'));
 
